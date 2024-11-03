8ff47eea00b352716c963e6f3a871ca013bd7d88
scamp531 <scampeche0531@gmail.com>
2024-11-03 12:07:52 -0500

    Update contact_us.js

--- JavaScript/contact_us.js
+++ JavaScript/contact_us.js
@@ -17,6 +17,31 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     }
   });
 
+//Ripple Effect Code Here
+const buttons = document.querySelectorAll('.ripple')
+
+buttons.forEach( button =>{
+    button.addEventListener('click', function(e){
+        const x = e.clientX;
+        const y = e.clientY;
+
+        const buttonTop = e.target.offsetTop;
+        const buttonLeft = e.target.offsetLeft;
+
+        const xInside = x - buttonLeft;
+        const yInside = y - buttonTop;
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle');
+        circle.style.top = yInside + 'px';
+        circle.style.left= xInside + 'px';
+
+        this.appendChild(circle);
+
+        setTimeout(() => circle.remove(), 500)
+    })
+})
+
 //Nav Bar Code
 const mobile = document.getElementById('mobile');
 const navLinks = document.querySelector('.nav_links');
